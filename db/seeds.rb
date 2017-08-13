f2dbf505580d3b52542d0cbb060a6bbb25cfc6d4
paul manot <paulmanot@gmail.com>
2017-08-13 23:14:40 +1000

    Create seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,30 @@
 
 10.times do
   Blog.create!(
-      title:
-      body:
+      title: Faker::ChuckNorris.fact,
+      body: Faker::Lorem.paragraph(2, true, 4)
     )
 end
+
+puts "10 blog posts created"
+
+5.times do
+  Skill.create!(
+      title: Faker::Superhero.power,
+      percent_utilized: Faker::Number.between(1, 100)
+    )
+end
+
+puts "5 skills created"
+
+9.times do
+  Portfolio.create!(
+      title: Faker::Internet.domain_name,
+      subtitle: Faker::Hacker.adjective,
+      body: Faker::Hacker.say_something_smart,
+      main_image: Faker::LoremPixel.image,
+      thumb_image: Faker::LoremPixel.image("50x60")
+    )
+end
+
+puts "9 portfolios created"
