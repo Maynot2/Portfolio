755846401f9e2b503bf97211f9e710b72a82cbc7
paul manot <paulmanot@gmail.com>
2017-08-16 22:33:58 +1000

    Customize portfolios routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   get '/about',   to: 'pages#about'
   get '/contact', to: 'pages#contact'
 
-  resources :portfolios
+  resources :portfolios, except: [:show, :new, :edit]
+  get 'portfolio/new',      to: 'portfolios#new',  as: 'new_portfolio'
+  get 'portfolio/:id',      to: 'portfolios#show', as: 'portfolio_show'
+  get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'edit_portfolio'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
