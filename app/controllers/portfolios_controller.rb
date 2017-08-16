e61bfc2d88c6229c89d1f0e19d979b053bbebd57
paul manot <paulmanot@gmail.com>
2017-08-16 10:13:03 +1000

    Integrate show new create  actions for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,32 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_item_params)
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to @portfolio_item,
+                      notice: "Portfolio Item successfuly created" }
+      else
+        format.html { render 'new',
+                      notice: "something went wrong please try again" }
+      end
+    end
+  end
+
+    private
+
+    def portfolio_item_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image,
+                                        :thumb_image)
+    end
 end
