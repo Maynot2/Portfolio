73dc44b9ad4143ff82952acc0c648476ab8d8af0
paul manot <paulmanot@gmail.com>
2017-08-16 12:19:34 +1000

    Integrate edit create destroy actions for portfolio_item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,14 +1,18 @@
 class PortfoliosController < ApplicationController
+  before_action :get_portfolio_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = Portfolio.all
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def new
     @portfolio_item = Portfolio.new
+  end
+
+  def edit
   end
 
   def create
@@ -24,7 +28,32 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def update
+    @portfolio_item.update(portfolio_item_params)
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to @portfolio_item,
+                      notice: "Portfolio Item successfuly updated" }
+      else
+        format.html { render 'new',
+                      notice: "something went wrong please try again" }
+      end
+    end
+  end
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url,
+                    notice: "Portfolio Item successfuly removed"}
+    end
+  end
+
     private
+
+    def get_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
 
     def portfolio_item_params
       params.require(:portfolio).permit(:title, :subtitle, :body, :main_image,
